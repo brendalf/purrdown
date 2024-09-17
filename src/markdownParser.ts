52aa0fdb823b8596133faffe4e41b89abc84be82
Breno Silva <brenophp@gmail.com>
2024-09-17 23:58:36 +0200

    feat: add block parser with tests

--- src/markdownParser.ts
+++ src/markdownParser.ts
@@ -1,42 +1,128 @@
-export enum BlockType {
-  H6,
-  H5,
-  H4,
-  H3,
-  H2,
-  H1,
-  NOTE,
-  PARAGRAPH,
-  IMAGE,
+export interface Block {}
+export interface Heading extends Block {
+  title: string;
+  size: number;
+}
+export interface Image extends Block {
+  alt: string;
+  url: string;
+}
+export interface Paragraph extends Block {
+  content: string;
+}
+export interface Note extends Paragraph {}
+export interface Code extends Block {
+  script: string;
+  language?: string;
 }
 
-export type Block = {
-  type: BlockType;
-  content: string;
-};
-
-export const defineBlocks = (line: string): Block => {
-  if (line.startsWith("######")) {
-    return { type: BlockType.H6, content: line.slice(6).trimStart() };
-  } else if (line.startsWith("#####")) {
-    return { type: BlockType.H5, content: line.slice(5).trimStart() };
-  } else if (line.startsWith("####")) {
-    return { type: BlockType.H4, content: line.slice(4).trimStart() };
-  } else if (line.startsWith("###")) {
-    return { type: BlockType.H3, content: line.slice(3).trimStart() };
-  } else if (line.startsWith("##")) {
-    return { type: BlockType.H2, content: line.slice(2).trimStart() };
-  } else if (line.startsWith("#")) {
-    return { type: BlockType.H1, content: line.slice(1).trimStart() };
-  } else if (line.startsWith("![")) {
-    return { type: BlockType.IMAGE, content: line.trimStart() };
-  } else if (line.startsWith(">")) {
-    return { type: BlockType.NOTE, content: line.slice(1).trimStart() };
-  } else {
-    return { type: BlockType.PARAGRAPH, content: line.trimStart() };
-  }
-};
-
 export const parseMarkdown = (content: string): Block[] => {
-  return content.split("\n").map(defineBlocks);
+  const lines = content.split("\n");
+  const blocks: Block[] = [];
+
+  for (let i = 0; i < lines.length; i++) {
+    let line = lines[i].trim();
+
+    if (line == "") {
+      blocks.push({});
+      continue;
+    }
+
+    if (line.startsWith("######")) {
+      blocks.push({ title: line.slice(6).trim(), size: 6 });
+      continue;
+    }
+
+    if (line.startsWith("#####")) {
+      blocks.push({ title: line.slice(5).trim(), size: 5 });
+      continue;
+    }
+
+    if (line.startsWith("####")) {
+      blocks.push({ title: line.slice(4).trim(), size: 4 });
+      continue;
+    }
+
+    if (line.startsWith("###")) {
+      blocks.push({ title: line.slice(3).trim(), size: 3 });
+      continue;
+    }
+
+    if (line.startsWith("##")) {
+      blocks.push({ title: line.slice(2).trim(), size: 2 });
+      continue;
+    }
+
+    if (line.startsWith("#")) {
+      blocks.push({ title: line.slice(1).trim(), size: 1 });
+      continue;
+    }
+
+    if (line.startsWith("```")) {
+      let language = line.slice(3).trim();
+      let script = "";
+
+      let j = i + 1;
+      while (!lines[j].trim().startsWith("```")) {
+        script += lines[j] + "\n";
+        j++;
+      }
+
+      i = j;
+
+      blocks.push({ script: script, language: language });
+      continue;
+    }
+
+    if (line.startsWith("![")) {
+      let alt = "";
+      let url = "";
+
+      let max = line.length;
+      let j = 2;
+
+      while (line[j] != "]" && j < max) {
+        alt += line[j];
+        j++;
+      }
+
+      if (j == max) {
+        blocks.push({ content: line });
+        continue;
+      }
+
+      while (line[j] != "(" && j < max) {
+        j++;
+      }
+
+      if (j == max) {
+        blocks.push({ content: line });
+        continue;
+      }
+
+      j++;
+
+      while (line[j] != ")" && j < max) {
+        url += line[j];
+        j++;
+      }
+
+      if (j == max) {
+        blocks.push({ content: line });
+        continue;
+      }
+
+      blocks.push({ alt: alt, url: url });
+      continue;
+    }
+
+    if (line.startsWith(">")) {
+      blocks.push({ content: line.slice(1).trim() });
+      continue;
+    }
+
+    blocks.push({ content: line.slice(0).trim() });
+  }
+
+  return blocks;
 };
--- tests/markdownParser.test.ts
+++ tests/markdownParser.test.ts
@@ -1,51 +1,55 @@
-import { parseMarkdown, BlockType, Block } from "../src/markdownParser";
+import { parseMarkdown, Block } from "../src/markdownParser";
 
 describe("parseMarkdown", () => {
   it("should correctly parse markdown into blocks", () => {
     const markdown = `
 # Heading 1
+#       Heading 1
 ## Heading 2
 ### Heading 3
 #### Heading 4
 ##### Heading 5
-###### Heading 6
+###### Heading 6    
+
+\`\`\`python
+def main():
+  pass
+\`\`\`
 
 > This is a note
 
 ![Alt text](image_url)
+![Alt text  
+![Alt text]    
+![Alt text]    (image_url
+![Alt text]    (image_url)     
 
 This is a paragraph.
     `.trim();
 
     const expectedBlocks: Block[] = [
-      { type: BlockType.H1, content: "Heading 1" },
-      { type: BlockType.H2, content: "Heading 2" },
-      { type: BlockType.H3, content: "Heading 3" },
-      { type: BlockType.H4, content: "Heading 4" },
-      { type: BlockType.H5, content: "Heading 5" },
-      { type: BlockType.H6, content: "Heading 6" },
-      { type: BlockType.NOTE, content: "This is a note" },
-      { type: BlockType.IMAGE, content: "![Alt text](image_url)" },
-      { type: BlockType.PARAGRAPH, content: "This is a paragraph." },
-    ];
-
-    const result = parseMarkdown(markdown);
-    expect(result).toEqual(expectedBlocks);
-  });
-
-  it("should handle empty lines as paragraphs", () => {
-    const markdown = `
-# Heading 1
-
-This is a paragraph.
-
-`.trim();
-
-    const expectedBlocks: Block[] = [
-      { type: BlockType.H1, content: "Heading 1" },
-      { type: BlockType.PARAGRAPH, content: "" },
-      { type: BlockType.PARAGRAPH, content: "This is a paragraph." },
-      { type: BlockType.PARAGRAPH, content: "" },
+      { title: "Heading 1", size: 1 },
+      { title: "Heading 1", size: 1 },
+      { title: "Heading 2", size: 2 },
+      { title: "Heading 3", size: 3 },
+      { title: "Heading 4", size: 4 },
+      { title: "Heading 5", size: 5 },
+      { title: "Heading 6", size: 6 },
+      {},
+      {
+        script: "def main():\n  pass\n",
+        language: "python",
+      },
+      {},
+      { content: "This is a note" },
+      {},
+      { alt: "Alt text", url: "image_url" },
+      { content: "![Alt text" },
+      { content: "![Alt text]" },
+      { content: "![Alt text]    (image_url" },
+      { alt: "Alt text", url: "image_url" },
+      {},
+      { content: "This is a paragraph." },
     ];
 
     const result = parseMarkdown(markdown);
